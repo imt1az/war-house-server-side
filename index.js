3d0e0e71a4d7610461d23d09b9cb250184a8e17b
Imtiaz Ahmed Chowdhury <imtiazemon625@gmail.com>
2022-05-08 22:39:06 +0600

    Delete

--- index.js
+++ index.js
@@ -104,12 +104,12 @@ async function run() {
           res.send(result);
     })
     //Delete
-    app.delete('/inventory/:id',async(req,res)=>{
-        const id = req.params.id;
-      const query = {_id:ObjectId(id)}
-      const result = await ProductCollection.deleteOne(query);
-      res.send(result);
-    })
+    // app.delete('/inventory/:id',async(req,res)=>{
+    //     const id = req.params.id;
+    //   const query = {_id:ObjectId(id)}
+    //   const result = await ProductCollection.deleteOne(query);
+    //   res.send(result);
+    // })
 
 
   } 
